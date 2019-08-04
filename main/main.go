37113954b847b581c733533331d6ad597bba93fe
Brandon Glowatski <glowatsk@gmail.com>
2019-08-04 14:18:50 +0000

    Adding static HTTP files to display JSON

--- main/main.go
+++ main/main.go
@@ -3,10 +3,29 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
 func main() {
+	parseJSON()
+	serveHTTP()
+}
+
+func serveHTTP() {
+	tmpl := template.Must(template.ParseFiles("/home/glowatsk/go/src/github.com/glowatsk/go-JSON-editor/tpl.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := "You are getting data"
+		tmpl.Execute(w, data)
+		fmt.Println(r)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func parseJSON() {
 	f, err := ioutil.ReadFile("/home/glowatsk/go/src/github.com/glowatsk/go-JSON-editor/hard.json")
 	if err != nil {
 		panic(err)
@@ -16,10 +35,10 @@ func main() {
 	err = json.Unmarshal(f, &i)
 
 	fetchValue(i)
-
 }
 
 func fetchValue(value interface{}) {
+	fmt.Printf("%v \n", value)
 	switch value.(type) {
 	case string:
 		fmt.Printf("%v is an interface \n ", value)
@@ -28,13 +47,14 @@ func fetchValue(value interface{}) {
 	case float64:
 		fmt.Printf("%v is float64 \n ", value)
 	case []interface{}:
-		fmt.Printf("%v is a slice of interface \n ", value)
+		// fmt.Printf("%v is a slice of interface \n ", value)
 		for _, v := range value.([]interface{}) { // use type assertion to loop over []interface{}
 			fetchValue(v)
 		}
 	case map[string]interface{}:
-		fmt.Printf("%v is a map \n ", value)
-		for _, v := range value.(map[string]interface{}) { // use type assertion to loop over map[string]interface{}
+		// fmt.Printf("%v is a map \n ", value)
+		for k, v := range value.(map[string]interface{}) { // use type assertion to loop over map[string]interface{}
+			fmt.Printf("%v \n", k)
 			fetchValue(v)
 		}
 	default:
